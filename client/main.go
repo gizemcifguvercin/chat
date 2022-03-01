8d3c5a8f98c841dc64f738e30b8a11d44d056b07
Gizem Cifgüvercin <gizem.cifguvercin@getir.com>
2022-03-01 12:08:23 +0300

    comment removed

--- client/main.go
+++ client/main.go
@@ -39,13 +39,3 @@ func main() {
 	}
 	log.Printf("server response : %s", r.GetMessage())
 }
-
-/*
-go run main.go --name=Gizem
-*/
-
-/*
-protoc --go_out=. --go_opt=paths=source_relative \
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    proto/chat.proto
-*/
